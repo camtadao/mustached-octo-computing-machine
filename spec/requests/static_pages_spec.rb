51882f2802bb3393cc2f31a99a992543b78e413d
Cameron Crawford <cameron@nonfatmedia.com>
2014-02-13 15:01:58 -0800

    finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ describe "Static pages" do
 		  	visit '/static_pages/home'
 		  	expect(page).to have_content('Sample App')
 		end
+    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
 	end
 
 	describe "About page" do
@@ -15,6 +20,11 @@ describe "Static pages" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
 		end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
 	end
 
 	describe "Help page" do
@@ -23,6 +33,11 @@ describe "Static pages" do
 			visit '/static_pages/help'
 		  	expect(page).to have_content('Help')
 		end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
 	end
 
 end
