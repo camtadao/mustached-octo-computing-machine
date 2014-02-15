5c929dfbee0f5818e53e431b9ed0f79d2bbcf60b
Cameron Crawford <cameron@nonfatmedia.com>
2014-02-14 16:10:33 -0800

    End of layout and routes. (chapter 5)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,7 +9,7 @@ describe "Static pages" do
 		before { visit root_path }
 
 		it { should have_content('Sample App') }
-		it { should have_title("#{base_title}") }
+		it { should have_title(full_title('')) }
 		it { should_not have_title('| Home') }
 	end
 
@@ -17,20 +17,20 @@ describe "Static pages" do
 		before { visit about_path }
 
 		it { should have_content('About Us') }
-    it { should have_title("#{base_title} | About Us") }
+    it { should have_title(full_title('About Us')) }
 	end
 
 	describe "Help page" do
 		before { visit help_path }
 
 		it { should have_content('Help') }
-    it { should have_title("#{base_title} | Help") }
+    it { should have_title(full_title('Help')) }
 	end
 
   describe "Contact page" do
   	before { visit contact_path }
  
  		it { should have_content('Contact') }
-    it { should have_title("#{base_title} | Contact") }
+    it { should have_title(full_title('Contact')) }
   end
 end
