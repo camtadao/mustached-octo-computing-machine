047a1b684dd110b418827f913cd53de673fba618
cameron crawford <cameron@nonfatmedia.com>
2014-02-17 16:40:43 -0800

    start of user profile

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Nola Ramos-Crawford"
+    email    "crawford.cameron+nola@gmail.com"
+    password "whatev"
+    password_confirmation "whatev"
+  end
+end
